01cf6afe87e8c745a6c9275867506b593b507f9e
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-01-09 19:51:51 -0300

    finalização

--- Js/LogicaPT3/carrinho-compras/js/app.js
+++ Js/LogicaPT3/carrinho-compras/js/app.js
@@ -1,26 +1,37 @@
 let totalGeral = 0;
+limpar();
 
-function adicionar(){
+function adicionar() {
     //recuperar valores: nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
     //calcular o preço, e o subtotal
     let preco = quantidade * valorUnitario;
     //adicionar no carrinho
     let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML =  carrinho.innerHTML +`<section class="carrinho__produtos__produto">
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
         <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`;
     //atualizar o grande total
     totalGeral = totalGeral + preco;
     let campoTotal = document.getElementById('valor-total');
     campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
 
-function limpar(){
-    document.getElementById('lista-produtos').value = '';
-    document.getElementById('quantidade').value = '';
-    
+function limpar() {
+totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').innerHTML = 'R$ 0';
+
 }
