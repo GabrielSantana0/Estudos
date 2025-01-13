3e7abcb3813863aba08de2d2f062e0b400eaa982
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-01-13 16:45:08 -0300

    Update app.js

--- Js/LogicaPT3/ingresso/js/app.js
+++ Js/LogicaPT3/ingresso/js/app.js
@@ -4,6 +4,10 @@ function comprar() {
 
     if (tipoIngresso.value == 'pista'){
         comprarPista(quantidade);
+    } else if (tipoIngresso.value == 'superior') {
+        comprarSuperior(quantidade);
+    } else {
+        comprarInferior(quantidade);
     }
 }
 
@@ -17,6 +21,34 @@ function comprarPista(quantidade){
     else {
         pista = pista - quantidade;
         document.getElementById('qtd-pista').textContent = pista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(quantidade){
+    let superior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (quantidade > superior) {
+        alert('Quantidade indisponível para tipo superior');
+    } else if (quantidade < 0) {
+    alert('Quantidade indisponível para tipo superior');
+    } 
+    else {
+        superior = superior - quantidade;
+        document.getElementById('qtd-superior').textContent = superior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(quantidade){
+    let inferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (quantidade > inferior) {
+        alert('Quantidade indisponível para tipo inferior');
+    } else if (quantidade < 0) {
+    alert('Quantidade indisponível para tipo inferior');
+    } 
+    else {
+        inferior = inferior - quantidade;
+        document.getElementById('qtd-inferior').textContent = inferior;
         alert('Compra realizada com sucesso!');
     }
 }
