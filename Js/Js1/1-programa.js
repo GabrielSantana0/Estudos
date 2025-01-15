8482a17a10e116fb35a086b3214ae44dc7b86eeb
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-01-15 18:36:45 -0300

    update

--- Js/Js1/1-programa.js
+++ Js/Js1/1-programa.js
@@ -1,1 +1,11 @@
-console.log('ola, mundo')
+console.log('Meu primeiro programa: Trabalhando com variaveis')
+
+const idade = 18;
+console.log('idades:')
+console.log(idade)
+console.log(idade+2)
+console.log(idade-2)
+console.log(idade/2)
+
+const idadeSomada = idade+10;
+console.log(idadeSomada);
