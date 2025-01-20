1579c82146817646d68af65fd914b00ba2ceb5e3
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-01-20 16:05:45 -0300

    Update 8-loops.js

--- Js/Js1/8-loops.js
+++ Js/Js1/8-loops.js
@@ -18,21 +18,13 @@ console.log(listasDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let  contador = 0;
+let destinoExiste = false;
 while(contador<3){
     if(listasDeDestinos[contador] == destino){
-        console.log('Destino existe');
-    }else {
-        console.log('Destino não existe!');
+        destinoExiste = true;
+        break;
     }
     contador += 1;
 }
 
-console.log('Embarque: \n\n')
-if (idadeComprador >= 18 && temPassagemComprada){
-    console.log('Boa viagem!')
-} else {
-    console.log('Você não pode embarcar!')
-}
-
-listasDeDestinos.splice(1, 1) //removendo item
-console.log(listasDeDestinos);
+console.log('Destino existe: ', destinoExiste);
