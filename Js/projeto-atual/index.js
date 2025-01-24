0dbadb78fa41007c957e562912ea8bef7f54c86d
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-01-24 05:34:34 -0300

    Update index.js

---
+++ Js/projeto-atual/index.js
@@ -0,0 +1,16 @@
+const cliente1Nome = 'Ricardo';
+const cliente1Cpf = 11113333322;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2nome = 'Alice';
+const cliente2Cpf = 888238421;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+const cliente3nome = 'Alice';
+const cliente3Cpf = 888238421;
+const client3RG = assa
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+console.log(cliente1Nome);
