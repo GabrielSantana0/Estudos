5f65bb57e16f5433876174e173dc57b67335fdf8
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-01-26 16:54:27 -0300

    Update index.js

--- Js/projeto-atual/index.js
+++ Js/projeto-atual/index.js
@@ -1,26 +1,40 @@
-class cliente{
+class cliente {
     nome;
     cpf;
+}
+
+class contaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new cliente();
 const cliente2 = new cliente();
-const cliente3 = new cliente();
 
 cliente1.nome = 'Ricardo';
 cliente1.cpf = 11113333322;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 cliente2.nome = 'Alice';
 cliente2.cpf = 888238421;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-cliente3.nome = 'Rafael';
-cliente3.cpf = 888238421;
-cliente3.agencia = 1001;
-cliente3.saldo = 0;
-console.log(cliente1, cliente2, cliente3);
+const contaCorrenteRicardo = new contaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(50);
+
+
+
+console.log(contaCorrenteRicardo.saldo);
+console.log(cliente1);
+console.log(cliente2);
