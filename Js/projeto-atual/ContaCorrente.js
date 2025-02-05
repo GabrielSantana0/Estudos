d96f989a3adbb28ebb3f1e948744ea90b3607632
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-02-05 16:14:37 -0300

    update

--- Js/projeto-atual/ContaCorrente.js
+++ Js/projeto-atual/ContaCorrente.js
@@ -1,9 +1,26 @@
+import { cliente } from "./Cliente.js";
+
 export class contaCorrente {
     agencia;
-    cliente;
+    _cliente;
+
+    set cliente(novoValor) {
+        if (novoValor instanceof cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
 
     // #saldo = 0;
     _saldo = 0;
+
+    get saldo(){
+        return this._saldo;
+    }
+
 
     sacar(valor) {
         if (this._saldo >= valor) {
@@ -12,14 +29,14 @@ export class contaCorrente {
         }
     }
 
-    depositar(valor){
-        if(valor <= 0){
+    depositar(valor) {
+        if (valor <= 0) {
             return;
         }
         this._saldo += valor;
     }
 
-    trasferir(valor, conta){
+    trasferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
     }
