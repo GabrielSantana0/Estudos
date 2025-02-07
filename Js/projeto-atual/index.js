d6d3e82cc922bb99e53672a004ffb63cedcea25d
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-02-07 16:45:29 -0300

    Update index.js

--- Js/projeto-atual/index.js
+++ Js/projeto-atual/index.js
@@ -1,11 +1,11 @@
 import { cliente } from './Cliente.js';
 import { contaCorrente } from './ContaCorrente.js';
 
-const cliente1 = new cliente('Ricardo', 11113333322);
+const cliente1 = new cliente('Ricardo', 62739154016);
 // cliente1.nome = 'Ricardo';
 // cliente1.cpf = 11113333322;
 
-const cliente2 = new cliente('Alice', 888238421);
+const cliente2 = new cliente('Alice', 13347386902);
 // cliente2.nome = 'Alice';
 // cliente2.cpf = 888238421;
 
@@ -19,3 +19,4 @@ const conta2 = new contaCorrente(cliente2, 102 );
 
 contaCorrenteRicardo.trasferir(200, conta2);
 console.log(contaCorrenteRicardo);
+console.log(conta2);
