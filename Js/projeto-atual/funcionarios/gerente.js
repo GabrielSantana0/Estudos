78bb6b79b90698430f77d9f928b02d14784d47ea
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-02-18 16:20:28 -0300

    update

---
+++ Js/projeto-atual/funcionarios/gerente.js
@@ -0,0 +1,5 @@
+export class gerente{
+    constructor(){
+
+    }
+}
--- Js/projeto-atual/index.js
+++ Js/projeto-atual/index.js
@@ -1,8 +1,7 @@
 import { cliente } from './Cliente.js';
-import { conta } from './conta.js';
-import { contaCorrente } from './ContaCorrente.js';
-import { contaPoupanca } from './contaPoupanca.js';
-import { contaSalario } from './contaSalario.js';
+import { contaCorrente } from './conta/ContaCorrente.js';
+import { contaPoupanca } from './conta/contaPoupanca.js';
+import { contaSalario } from './conta/contaSalario.js';
 
 
 const cliente1 = new cliente('Ricardo', 62739154016);
@@ -14,14 +13,10 @@ const cliente1 = new cliente('Ricardo', 62739154016);
 // cliente2.cpf = 888238421;
 
 const contaCorrenteRicardo = new contaCorrente(cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
-
 const contaPoupanca1 = new contaPoupanca(50, cliente1, 1001);
-contaPoupanca1.sacar(10);
 const contaSalario1 = new contaSalario(cliente1);
 contaSalario1.depositar(100);
 contaSalario1.sacar(10);
-console.log(contaSalario1);
 
-console.log(contaCorrenteRicardo);
-console.log(contaPoupanca1);
+
+console.log(contaSalario1);
