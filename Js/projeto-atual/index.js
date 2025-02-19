c24e5cd5f48dcf18b158b24574ba3f5b1134d3d1
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-02-19 16:27:42 -0300

    senha gerente e diretor

--- Js/projeto-atual/index.js
+++ Js/projeto-atual/index.js
@@ -1,8 +1,14 @@
 import { cliente } from './Cliente.js';
 import { gerente } from './funcionarios/gerente.js';
 import { diretor } from './funcionarios/diretor.js';
+import { sistemaAutentificacao } from './sistemaAutentificacao.js';
 
 const diretor1 =  new diretor('rodrigo', 10000, 12345678900);
-const gerente1 = new gerente('ricardo', 5000, 12342342901);
+diretor1.cadastrarSenha('123456')
 
-sistemaAutentificacao.login(diretor1, '123456789')
+const gerente1 = new gerente('ricardo', 5000, 12342342901);
+gerente1.cadastrarSenha('321')
+
+const estaLogado = sistemaAutentificacao.login(diretor1, '123456')
+
+console.log(estaLogado);
