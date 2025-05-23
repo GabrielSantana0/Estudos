af8e19ab95fb45cc75f9abc7cb9ae26bd644f743
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-05-23 13:28:25 -0300

    organização

--- Js/alura_books-projeto_inicial/app/main.js
+++ Js/alura_books-projeto_inicial/app/main.js
@@ -11,22 +11,3 @@ async function getBuscarLivrosDaAPI() {
     console.table(livros)
     exibirOsLivrosNaTela(livros)
 }
-
-function exibirOsLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-<div class="livro">
-<img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-<h2 class="livro__titulo">
-        Cangaceiro JavaScript:
-        Uma aventura no sertão da programação
-</h2>
-    <p class="livro__descricao">${livro.autor}</p>
-    <p class="livro__preco" id="preco">${livro.preco}</p>
-    <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-    </div>
-    </div>
-    `
-    })
-}
