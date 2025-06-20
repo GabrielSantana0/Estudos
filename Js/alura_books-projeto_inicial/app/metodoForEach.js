ea9be351492ff207b501e5e963d7df5b75e5dc7c
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-06-20 17:28:48 -0300

    finish

--- Js/alura_books-projeto_inicial/app/metodoForEach.js
+++ Js/alura_books-projeto_inicial/app/metodoForEach.js
@@ -1,9 +1,13 @@
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
+
 function exibirOsLivrosNaTela(listaDeLivros) {
-    elementoParaInserirLivros.innerHTML = '';
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach((livro) => {
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
         elementoParaInserirLivros.innerHTML += `
         <div class='livro'>
-        <img class='livro__imagens' src='${livro.imagem}'
+        <img class='${disponibilidade}' src='${livro.imagem}'
         alt='${livro.alt}' />
         <h2 class='livro__titulo'>
         ${livro.titulo}
@@ -14,7 +18,7 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         <span class='tag'>${livro.categoria}</span>
         </div>
     </div>
-        `;
+        `
     });
     
 }
