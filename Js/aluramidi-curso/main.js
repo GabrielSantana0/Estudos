be8135f52df283687d03091db805aca96c822868
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2025-06-28 17:42:24 -0300

    Update main.js

--- Js/aluramidi-curso/main.js
+++ Js/aluramidi-curso/main.js
@@ -1,8 +1,38 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (seletorAudio) {
+const elemento = document.querySelector(seletorAudio).play();
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play()
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor invalido')
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom
+let  contador = 0;
 
+
+for ( let contador = 0; contador < listaDeTeclas.length; contador ++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function(evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa')
+        }
+    }
+
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa')
+    }
+} 
